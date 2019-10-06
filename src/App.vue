d8cdc17ff2ffe1777122071ab1e304e6987d799a
Onur Cinar <onur.cinar@gmail.com>
2019-10-05 20:55:18 -0700

    Vue material  app structure.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,81 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <md-app md-waterfall md-mode="fixed">
+    <md-app-toolbar class="md-primary">
+      <div class="md-toolbar-section-start" v-if="!menuVisible">
+        <md-button class="md-icon-button" v-on:click="toggleMenu()">
+          <md-icon>menu</md-icon>
+        </md-button>
+        <span class="md-title">Parrent Parrot</span>
+      </div>
+
+      <div class="md-toolbar-section-end">
+        <md-button class="md-icon-button" v-on:click="addClip()">
+          <md-icon>add</md-icon>
+        </md-button>
+      </div>
+    </md-app-toolbar>
+
+    <md-app-drawer :md-active.sync="menuVisible">
+      <md-toolbar class="md-transparent" md-elevation="0">
+        <span class="md-title">Menu</span>
+
+        <div class="md-toolbar-section-end">
+          <md-button class="md-icon-button md-dense" v-on:click="toggleMenu()">
+            <md-icon>keyboard_arrow_left</md-icon>
+          </md-button>
+        </div>
+      </md-toolbar>
+
+      <md-list>
+        <md-list-item v-on:click="clearClips()">
+          <md-icon>clear</md-icon>
+          <span class="md-list-item-text">Clear</span>
+        </md-list-item>
+      </md-list>
+    </md-app-drawer>
+
+    <md-app-content>
+      <div class="md-layout">
+        <pp-clip-item v-for="clip in clips" v-bind:key="clip" />
+      </div>
+    </md-app-content>
+  </md-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PpClipItem from './components/PpClipItem.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    PpClipItem
+  },
+  data: function() {
+    return {
+      menuVisible: false,
+      clips: []
+    }
+  },
+  methods: {
+    toggleMenu: function() {
+      this.menuVisible = !this.menuVisible
+    },
+
+    addClip: function() {
+      this.clips.push({})
+    },
+
+    clearClips: function() {
+      this.clips.splice(0, this.clips.length)
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.md-app {
+  min-height: 100vh;
+  max-height: 100vh;
 }
+
 </style>
---
+++ src/components/PpClipItem.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="md-layout-item md-size-50">
+    <div class="pp-clip-item-content">1</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'pp-clip-item'
+}
+</script>
+
+<style>
+.pp-clip-item-content {
+  background-color: red;
+  margin: 2px;
+  height: 120px;
+  line-height: 120px;
+  text-align: center;
+}
+</style>
